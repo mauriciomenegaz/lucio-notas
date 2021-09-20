2f16d4b287514bfe7a2ed9b96aa7cadf4d48a355
Mauricio Menegaz <mauricio.menegaz@gmail.com>
2021-09-19 23:10:12 -0300

    second try

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import * as Tone from 'tone'
 
-//create a synth and connect it to the main output (your speakers)
-const synth = new Tone.Synth().toDestination();
+console.log("hello");
 
-//play a middle 'C' for the duration of an 8th note
-synth.triggerAttackRelease("C4", "8n");
+window.onButton = function () {
+  console.log("button")
+  //create a synth and connect it to the main output (your speakers)
+  const synth = new Tone.Synth().toDestination();
+  //play a middle 'C' for the duration of an 8th note
+  synth.triggerAttackRelease("C4", "8n");
+}
